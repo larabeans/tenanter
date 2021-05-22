f164179c7b0d071b0cbd624b73849fc3e586d437
Syed Ali Murtaza <ali@kazmi.me>
2021-05-22 14:00:14 +0500

    Adds tenant model

---
+++ Models/Tenant.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Containers\Vendor\Tenanter\Models;
+
+use App\Ship\Parents\Models\Model;
+
+class Tenant extends Model
+{
+    protected $fillable = [
+      'slug',
+      'name',
+      'is_active'
+    ];
+
+    protected $attributes = [
+
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    protected $casts = [
+
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * A resource key to be used by the the JSON API Serializer responses.
+     */
+    protected $resourceKey = 'tenants';
+}
