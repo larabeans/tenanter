711e63f315e9fe54e4a3c6220d39f65e2d212afd
Sajjad Naqvi <sajjad@orenda.systems>
2021-09-30 18:58:54 +0500

    call another task

--- Actions/CreateTenantAction.php
+++ Actions/CreateTenantAction.php
@@ -5,6 +5,7 @@ namespace App\Containers\Vendor\Tenanter\Actions;
 use App\Containers\Vendor\Tenanter\Models\Tenant;
 use App\Ship\Parents\Actions\Action;
 use App\Containers\Vendor\Tenanter\Tasks\CreateTenantTask;
+use App\Containers\Vendor\Tenanter\Tasks\CreateTenantUserTask;
 use App\Containers\Vendor\Tenanter\UI\API\Requests\CreateTenantRequest;
 
 
@@ -13,6 +14,8 @@ class CreateTenantAction extends Action
     public function run(CreateTenantRequest $request) : Tenant
     {
         //dd($request);
-        return app(CreateTenantTask::class)->run(null, $request->name, $request->is_active,$request->domain);
+        $tenant = app(CreateTenantTask::class)->run(null, $request->name, $request->is_active,$request->domain);
+        $user = app(CreateTenantUserTask::class)->run(true,  $tenant->id, $request->email,$request->password);
+        return $tenant;
     }
 }
