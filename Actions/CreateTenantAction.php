906aa5b949b98e268ef10afe89b77cebc5fa9e8d
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-12 16:04:50 +0500

    Updates Run Function Parameter

--- Actions/CreateTenantAction.php
+++ Actions/CreateTenantAction.php
@@ -11,11 +11,11 @@ use App\Containers\Vendor\Tenanter\UI\API\Requests\CreateTenantRequest;
 
 class CreateTenantAction extends Action
 {
-    public function run(CreateTenantRequest $request): Tenant
+    public function run(CreateTenantRequest $request) : Tenant
     {
         //dd($request);
-        $tenant = app(CreateTenantTask::class)->run(null, $request->name, $request->is_active, $request->domain);
-        $user = app(CreateTenantUserTask::class)->run(true, $tenant->id, $request->email, $request->password);
+        $tenant = app(CreateTenantTask::class)->run(null, $request->name, $request->is_active,$request->domain,$request->mode);
+        $user = app(CreateTenantUserTask::class)->run(true,  $tenant->id, $request->email,$request->password);
         return $tenant;
     }
 }
