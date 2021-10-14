b541936bcb63ec2694730e86fd2e2e48dbf774f0
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-14 11:39:11 +0500

    Add default tenant condition

--- Data/Seeders/TenantDefaultTenantsSeeder_3.php
+++ Data/Seeders/TenantDefaultTenantsSeeder_3.php
@@ -10,12 +10,15 @@ class TenantDefaultTenantsSeeder_3 extends Seeder
     public function run()
     {
         // Default Store Seeder ---------------------------------------------
-        app(CreateTenantTask::class)->run(
-            config('tenanter.default_id'),
-            'Default Tenant',
-            $isActive = true,
-            'www.defaulttenant.com',
-            'active'
-        );
+        if (config('tenanter.default_id')) {
+            app(CreateTenantTask::class)->run(
+                config('tenanter.default_id'),
+                'Default Tenant',
+                $isActive = true,
+                'www.defaulttenant.com',
+                'active'
+            );
+        }
+
     }
 }
