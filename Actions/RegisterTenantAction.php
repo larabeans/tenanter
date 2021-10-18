999a17139923fc6250e58b0050750fff142b3a1e
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-18 12:27:20 +0500

    Make new action

---
+++ Actions/RegisterTenantAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Containers\Vendor\Tenanter\Actions;
+
+use App\Containers\Vendor\Tenanter\Models\Tenant;
+use App\Ship\Parents\Actions\Action;
+use App\Containers\Vendor\Tenanter\Tasks\CreateTenantTask;
+use App\Containers\Vendor\Tenanter\Tasks\CreateTenantUserTask;
+use App\Containers\Vendor\Tenanter\UI\API\Requests\RegisterTenantRequest;
+
+
+class RegisterTenantAction extends Action
+{
+    public function run(RegisterTenantRequest $request) : Tenant
+    {
+        //dd($request);
+        $tenant = app(CreateTenantTask::class)->run(null, $request->name, $request->is_active,$request->domain,$request->mode);
+        $user = app(CreateTenantUserTask::class)->run(true,  $tenant->id, $request->email,$request->password);
+        return $tenant;
+    }
+}
