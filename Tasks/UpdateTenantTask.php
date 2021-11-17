fef3a551612b03cc54778b91606827416e327ea5
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-17 18:13:15 +0500

    Updates file to manage tenant admin and host

--- Tasks/UpdateTenantTask.php
+++ Tasks/UpdateTenantTask.php
@@ -2,18 +2,23 @@
 
 namespace App\Containers\Vendor\Tenanter\Tasks;
 
+use App\Containers\Vendor\Configurationer\Traits\IsHostAdminTrait;
 use App\Containers\Vendor\Tenanter\Data\Repositories\TenantRepository;
 use App\Ship\Exceptions\UpdateResourceFailedException;
 use App\Ship\Exceptions\NotAuthorizedResourceException;
 use App\Ship\Parents\Tasks\Task;
 use Exception;
 use App\Containers\Vendor\Tenanter\Traits\IsTenantAdminTrait;
+use Illuminate\Support\Str;
+use Illuminate\Validation\UnauthorizedException;
+use function PHPUnit\Framework\throwException;
 
 class UpdateTenantTask extends Task
 {
     use IsTenantAdminTrait;
+    use IsHostAdminTrait;
 
-    protected $repository;
+    protected TenantRepository $repository;
 
     public function __construct(TenantRepository $repository)
     {
@@ -22,14 +27,18 @@ class UpdateTenantTask extends Task
 
     public function run($id, $data)
     {
-        if ($this->isTenantAdmin($id)) {
-            try {
+        try {
+            if ($this->isTenantAdmin($id) || $this->isHostAdmin()) {
+                if (isset($data['name'])) {
+                    $data['slug'] = Str::slug($data['name']);
+                }
                 return $this->repository->update($data, $id);
-            } catch (Exception $exception) {
-                throw new UpdateResourceFailedException($exception);
+            } else {
+                throw new UnauthorizedException();
             }
-        } else {
-            throw new NotAuthorizedResourceException();
+        } catch (Exception $exception) {
+            throw new UpdateResourceFailedException($exception);
         }
+
     }
 }
