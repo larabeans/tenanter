251d808a920347f7436fcd11e58de38cee502b8e
Syed Ali Murtaza <ali@kazmi.me>
2021-11-20 14:00:00 +0500

    improves trait to check is tenant admin

--- Traits/IsTenantAdminTrait.php
+++ Traits/IsTenantAdminTrait.php
@@ -8,17 +8,6 @@ trait IsTenantAdminTrait
 {
     public function isTenantAdmin(string $tenant_id): bool
     {
-        $user = Auth::user();
-
-        if (sizeof($user->roles) == 0) {
-            return false;
-        } else {
-            foreach ($user->roles as $role) {
-                if ($role->name == "tenant-admin" && Auth::user()->tenant_id == $tenant_id) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        return Auth::user()->hasRole('tenant-admin') && Auth::user()->tenant_id == $tenant_id;
     }
 }
