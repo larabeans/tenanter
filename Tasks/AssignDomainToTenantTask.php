47fc93dc6e351349ea59d96456d2c747a7f402a7
Sajjad Naqvi <sajjad@orenda.systems>
2021-12-13 17:59:08 +0500

    Updates task to add tenant_id on registeration

--- Tasks/AssignDomainToTenantTask.php
+++ Tasks/AssignDomainToTenantTask.php
@@ -18,30 +18,31 @@ class AssignDomainToTenantTask extends Task
         $this->repository = $repository;
     }
 
-    public function run($name, $new = false)
+    public function run($tenant, $new = false)
     {
 
         $domain = null;
         $dnsHostname = null;
         $dnsCode = null;
+        $data = null;
 
         if ($new == false) {
             $hostDomain = config('tenanter.host_domains');
-            $domain = $name . '.' . $hostDomain[1];
+            $domain = $tenant->name . '.' . $hostDomain[1];
+            $data['tenant_id'] = $tenant->id;
         } else {
-            $domain = $name;
+            $domain = $tenant;
             $dnsHostname = Str::random(5) . '.' . $domain;
             $dnsCode = Str::random(14);
         }
 
-        $data = [
-            'domain' => $domain,
-            'is_active' => false,
-            'is_verified' => false,
-            'dns_verification_hostname' => $dnsHostname,
-            'dns_verification_code' => $dnsCode,
-            'verified_at' => null
-        ];
+        $data['domain'] = $domain;
+        $data['is_active'] = false;
+        $data['is_verified'] = false;
+        $data['dns_verification_hostname'] = $dnsHostname;
+        $data['dns_verification_code'] = $dnsCode;
+        $data['verified_at'] = null;
+
         try {
             return $this->repository->create($data);
         } catch (Exception $exception) {
